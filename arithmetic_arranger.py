bb12835c30b8acb2acff7d39544ef21231b04661
Ahmad Khidir <khidirahmad05@gmail.com>
2022-12-27 15:57:26 -0800

    testing with given example

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -47,3 +47,4 @@ def arithmetic_arranger(operations:list, with_answer=False):
 if __name__ == '__main__':
     arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
     arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+
